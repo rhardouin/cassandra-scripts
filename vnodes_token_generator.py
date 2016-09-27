cb6e6c9f885bdb5eae90f64e175ae5c2ab0712a3
Romain Hardouin <romain_hardouin@yahoo.fr>
2016-09-27 23:01:18 +0200

    Add --offset <int>
    Allow to specify a value (integer) to add to each token to avoid potential conflicts (e.g. 1).

--- vnodes_token_generator.py
+++ vnodes_token_generator.py
@@ -60,11 +60,12 @@ def generate_tokens(vnodes, num=None, server=None):
     return tokens_per_srv
 
 
-def generate_tokens(vnodes, num_srv):
+def generate_tokens(vnodes, num_srv, offset):
     """Generate evenly distributed interleaved tokens for each server.
 
     :param vnodes: int Number of vnodes per server
     :param num_srv: int Number of Cassandra servers
+    :param offset: int Value to add to each token
     :return: defaultdict key=server number, value=its tokens
     """
     num_tokens = vnodes * num_srv
@@ -72,7 +73,8 @@ def generate_tokens(vnodes, num_srv):
     current_srv = 0
 
     for i in range(num_tokens):
-        token = str(((2**64 / num_tokens) * i) - 2**63)
+        raw_token = ((2 ** 64 / num_tokens) * i) - 2 ** 63
+        token = str(raw_token + offset)
         srv2tokens[current_srv].append(token)
         current_srv += 1
 
@@ -109,6 +111,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(usage=__doc__)
     parser.add_argument('vnodes', type=int, help='Number of vnodes per server')
+    parser.add_argument('--offset', type=int, default=0,
+                        help='Value to add to each token, to avoid potential '
+                             'conflicts (e.g. 1). Default is 0 (no offset).')
     parser.add_argument('-i', '--indent', type=int, help='JSON indentation spaces (e.g. 4)')
 
     format_group = parser.add_mutually_exclusive_group()
@@ -130,7 +135,7 @@ if __name__ == '__main__':
     else:
         sys.exit("Specify number of servers or hosts file")
 
-    tokens_per_srv = generate_tokens(args.vnodes, num_servers)
+    tokens_per_srv = generate_tokens(args.vnodes, num_servers, args.offset)
 
     if args.servers:
         tokens = tokens_per_srv.values()
