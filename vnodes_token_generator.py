da467fa3d0759afa7aece192f5eafb2106a64ad5
Romain Hardouin <romain_hardouin@yahoo.fr>
2016-09-29 18:25:12 +0200

    Add simple text output
    Update documentation

--- vnodes_token_generator.py
+++ vnodes_token_generator.py
@@ -5,24 +5,14 @@ Vnodes Murmur3 tokens generator: generate evenly distributed initial tokens for 
 
 `initial_token` can be set in cassandra.yaml whith vnodes by using comma separated values.
 
-Here are some usage examples::
+Example::
 
-    ./vnodes_token_generator.py --json --indent 2 --servers hosts.txt 4
+    ./vnodes_token_generator.py --json --indent 2 --servers hosts_interleaved_racks.txt 4
     {
-      "192.168.128.1": "-9223372036854775808,-4611686018427387905,-2,4611686018427387901",
-      "192.168.128.2": "-7686143364045646507,-3074457345618258604,1537228672809129299,6148914691236517202",
-      "192.168.128.3": "-6148914691236517206,-1537228672809129303,3074457345618258600,7686143364045646503"
+      "192.168.1.1": "-9223372036854775808,-4611686018427387905,-2,4611686018427387901",
+      "192.168.2.1": "-7686143364045646507,-3074457345618258604,1537228672809129299,6148914691236517202",
+      "192.168.3.1": "-6148914691236517206,-1537228672809129303,3074457345618258600,7686143364045646503"
     }
-
-    ./vnodes_token_generator.py --servers hosts.txt 4
-    Server 192.168.128.22 -> initial_token: -7686143364045646507,-3074457345618258604,1537228672809129299,6148914691236517202
-    Server 192.168.128.23 -> initial_token: -6148914691236517206,-1537228672809129303,3074457345618258600,7686143364045646503
-    Server 192.168.128.21 -> initial_token: -9223372036854775808,-4611686018427387905,-2,461168601842738790
-
-    ./vnodes_token_generator.py -n 3 4
-    Server 0 -> initial_token: -9223372036854775808,-4611686018427387905,-2,4611686018427387901
-    Server 1 -> initial_token: -7686143364045646507,-3074457345618258604,1537228672809129299,6148914691236517202
-    Server 2 -> initial_token: -6148914691236517206,-1537228672809129303,3074457345618258600,76861433640456465033
 
 See README for a real world example with interleaved racks.
 """
@@ -59,7 +49,7 @@ def show_cass_yaml(tokens_per_srv):
     :param tokens_per_srv: dict key=server number/hostname, value=its tokens
     """
     for index_or_hostname, srv_tokens in tokens_per_srv.items():
-        print("Server %s -> initial_token: %s" % (index_or_hostname, ','.join(srv_tokens)))
+        print("%s initial_token: %s" % (index_or_hostname, ','.join(srv_tokens)))
 
 
 def show_json(tokens_per_srv, indent=None):
@@ -74,12 +64,21 @@ def show_json(tokens_per_srv, indent=None):
         tokens_databag[srv] = ','.join(srv_tokens)
     print(json.dumps(tokens_databag, sort_keys=True, indent=indent))
 
+
+def show_text(tokens_per_srv):
+    """Print space separated tokens
+
+    :param tokens_per_srv: dict key=server number/hostname, value=its tokens
+    """
+    for index_or_hostname, srv_tokens in tokens_per_srv.items():
+        print("%s %s" % (index_or_hostname, ' '.join(srv_tokens)))
+
 if __name__ == '__main__':
     import argparse
     import json.tool
     import sys
 
-    parser = argparse.ArgumentParser(usage=__doc__)
+    parser = argparse.ArgumentParser()
     parser.add_argument('vnodes', type=int, help='Number of vnodes per server')
     parser.add_argument('--offset', type=int, default=0,
                         help='Value to add to each token, to avoid potential '
@@ -87,8 +86,13 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--indent', type=int, help='JSON indentation spaces (e.g. 4)')
 
     format_group = parser.add_mutually_exclusive_group()
-    format_group.add_argument('-j', '--json', action='store_true', help='JSON output')
-    format_group.add_argument('-y', '--yaml', action='store_true', help='initial_token for cassandra.yaml')
+    format_group.add_argument('-j', '--json',
+                              action='store_true', help='JSON output')
+    format_group.add_argument('-y', '--yaml', action='store_true',
+                              help='initial_token for cassandra.yaml')
+    format_group.add_argument('-t', '--text', action='store_true',
+                              help='Space separated values. First column: IP '
+                                   'address, then one column per token')
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-n', '--num', type=int, help='Number of Cassandra servers')
@@ -113,5 +117,7 @@ if __name__ == '__main__':
 
     if args.json:
         show_json(tokens_per_srv, indent=args.indent)
-    else:
+    elif args.yaml:
         show_cass_yaml(tokens_per_srv)
+    else:
+        show_text(tokens_per_srv)
